77d3bac88f16b754d6780515b61083d06ad725b8
MichaelGlad <0547987140g>
2016-07-03 20:35:59 +0300

    first app

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,13 @@ public class Main {
         int x = 8; //4 bytes
         byte y = 100; //1 byte -128 to 127
         short b = 55; //2 bytes
-        long c = 758943; //8 bytes
+        long l = 758943L; //8 bytes
+
+        float fl = 234.66f;//4 bytes heve to be f after the num
+        double d = 897324.9283;//8 bytes
+
+        char c = 'a';//one bytes one char per char heve to be in ' '
+
 
     }
 }
