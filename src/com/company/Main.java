7650eb542f1e93e7e8561f93b5a022901f457060
MichaelGlad <0547987140g>
2016-07-03 21:04:54 +0300

    first app

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,5 +14,18 @@ public class Main {
         char c = 'a';//one bytes one char per char heve to be in ' '
         boolean f = true;//one byte for meny booleans
 
+        String myString = "hello";// the name of the variabal cant start with num and cant use @#$%
+        myString = "world";//change the var
+
+        x = (5 + 2) * 2;//seder peulot heshbon
+        x = 5 / 2;//result is 2
+
+        f = 5 < 8;//check if true of folse
+        System.out.println(f);
+        System.out.println(5 < 8);//same same
+
+
+
+
     }
 }
