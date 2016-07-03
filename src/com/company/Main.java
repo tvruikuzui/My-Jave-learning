76b86f6185977b41348ed6b36bf8583d9904f637
MichaelGlad <0547987140g>
2016-07-03 20:24:07 +0300

    first app

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int x = 8; //4 bytes
+        byte y = 100; //1 byte -128 to 127
+        short b = 55; //2 bytes
+        long c = 758943; //8 bytes
 
     }
 }
